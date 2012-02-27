2fc860005531e000d3b5098593f11d31d2f83e21
Neil Roberts <neil@linux.intel.com>
2012-02-27 16:40:27 +0000

    Add the copyright headers to the source and #include "config.h"

--- src/test-wayland-surface.c
+++ src/test-wayland-surface.c
@@ -1,3 +1,26 @@
+/*
+ * test-wayland-surface
+ *
+ * An example Wayland compositor using Clutter
+ *
+ * Copyright (C) 2012  Intel Corporation.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
 #define COGL_ENABLE_EXPERIMENTAL_2_0_API
 #include <clutter/clutter.h>
 #include <clutter/wayland/clutter-wayland-compositor.h>
--- src/tws-compositor.h
+++ src/tws-compositor.h
@@ -1,3 +1,24 @@
+/*
+ * test-wayland-surface
+ *
+ * An example Wayland compositor using Clutter
+ *
+ * Copyright (C) 2012  Intel Corporation.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __TWS_COMPOSITOR_H__
 #define __TWS_COMPOSITOR_H__
 
